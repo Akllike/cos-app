97513d8adb266273e16626d7f8024b1f993aa0f1
Akllike <33116@list.ru>
2024-11-09 14:58:46 +0300

    09.11.2024-3

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -23,6 +23,6 @@ class SearchController extends Controller
 
         $title = 'Результаты поиска - ' . $request->input('name') . ' | ShaR';
 
-        return view('search', compact('muses', 'gels', 'scrabs', 'title'));
+        return view('search', compact('muses', 'gels', 'scrabs', 'title', 'oils'));
     }
 }
