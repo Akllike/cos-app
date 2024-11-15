e0d0b0312e540332d800abeabaa3f587b20e35a7
Akllike <33116@list.ru>
2024-11-15 16:12:31 +0300

    15.11.2024-2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,59 +71,59 @@
     </ul>
 
     <div id="list-muses" class="mt-4 d-flex flex-column">
-        @foreach($muses as $muse)
+        @foreach($muses as $item)
             <div class="mt-2 d-flex flex-wrap h-auto align-items-center justify-content-between border" style="height: 50px">
                 <div class="d-flex flex-row">
-                    <p class="m-2">id: {{ $muse['id'] }}</p> <p class="m-2">{{ $muse['name'] }}</p>
-                    <p class="m-2">({{ $muse['volume'] }} мл)</p>
+                    <p class="m-2">id: {{ $item['id'] }}</p> <p class="m-2">{{ $item['name'] }}</p>
+                    <p class="m-2">({{ $item['volume'] }} мл)</p>
                 </div>
                 <div class="d-flex">
-                    <button id="btn-edit-muse-{{ $muse['id'] }}" class="btn btn-primary m-1">Редактировать</button>
+                    <button id="btn-edit-muse-{{ $item['id'] }}" class="btn btn-primary m-1">Редактировать</button>
                     <form action="{{ url('admin/delete/') }}" method="POST" class="m-1">
                         @csrf
                         <input type="hidden" name="group-name" value="1">
-                        <input type="hidden" name="id" value="{{ $muse['id'] }}">
+                        <input type="hidden" name="id" value="{{ $item['id'] }}">
                         <button class="btn btn-primary">Удалить</button>
                     </form>
                 </div>
             </div>
-            <div id="toggle-muse-{{ $muse['id'] }}" style="display: none">
+            <div id="toggle-muse-{{ $item['id'] }}" style="display: none">
                 <div class="mt-2 d-flex align-items-center justify-content-between border-1">
                     <form action="{{ url('admin/edit/') }}" method="POST" class="row g-3 needs-validation">
                         @csrf
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Группа</span>
-                            <input class="form-control" type="text" id="group-name" value="{{ $muse['category'] }}" name="group-name"><br><br>
+                            <input class="form-control" type="text" id="group-name" value="{{ $item['category'] }}" name="group-name"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">id</span>
-                            <input class="form-control" type="text" id="id" value="{{ $muse['id'] }}" name="id"><br><br>
+                            <input class="form-control" type="text" id="id" value="{{ $item['id'] }}" name="id"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Заголовок</span>
-                            <input type="text" name="name" value="{{ $muse['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
+                            <input type="text" name="name" value="{{ $item['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Описание</span>
-                            <input type="text" name="description" value="{{ $muse['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
+                            <input type="text" name="description" value="{{ $item['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Состав</span>
-                            <input type="text" name="composition" value="{{ $muse['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
+                            <input type="text" name="composition" value="{{ $item['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
                         </div>
 
                         <div class="input-group mb-3">
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Цена</span>
-                                <input type="text" name="price" value="{{ $muse['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
+                                <input type="text" name="price" value="{{ $item['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Объем</span>
-                                <input type="text" name="volume" value="{{ $muse['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
+                                <input type="text" name="volume" value="{{ $item['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
                             </div>
                         </div>
 
@@ -145,59 +145,59 @@
     </div>
 
     <div id="list-gels" class="mt-4 d-none flex-column">
-        @foreach($gels as $gel)
+        @foreach($gels as $item)
             <div class="mt-2 d-flex flex-wrap h-auto align-items-center justify-content-between border" style="height: 50px">
                 <div class="d-flex flex-row">
-                    <p class="m-2">id: {{ $gel['id'] }}</p> <p class="m-2">{{ $gel['name'] }}</p>
-                    <p class="m-2">({{ $gel['volume'] }} мл)</p>
+                    <p class="m-2">id: {{ $item['id'] }}</p> <p class="m-2">{{ $item['name'] }}</p>
+                    <p class="m-2">({{ $item['volume'] }} мл)</p>
                 </div>
                 <div class="d-flex">
-                    <button id="btn-edit-gel-{{ $gel['id'] }}" class="btn btn-primary m-1">Редактировать</button>
+                    <button id="btn-edit-gel-{{ $item['id'] }}" class="btn btn-primary m-1">Редактировать</button>
                     <form action="{{ url('admin/delete/') }}" method="POST" class="m-1">
                         @csrf
                         <input type="hidden" name="group-name" value="2">
-                        <input type="hidden" name="id" value="{{ $gel['id'] }}">
+                        <input type="hidden" name="id" value="{{ $item['id'] }}">
                         <button class="btn btn-primary">Удалить</button>
                     </form>
                 </div>
             </div>
-            <div id="toggle-gel-{{ $gel['id'] }}" style="display: none">
+            <div id="toggle-gel-{{ $item['id'] }}" style="display: none">
                 <div class="mt-2 d-flex align-items-center justify-content-between border-1">
                     <form action="{{ url('admin/edit/') }}" method="POST" class="row g-3 needs-validation">
                         @csrf
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Группа</span>
-                            <input class="form-control" type="text" id="group-name" value="{{ $gel['category'] }}" name="group-name"><br><br>
+                            <input class="form-control" type="text" id="group-name" value="{{ $item['category'] }}" name="group-name"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">id</span>
-                            <input class="form-control" type="text" id="group-name" value="{{ $gel['id'] }}" name="id"><br><br>
+                            <input class="form-control" type="text" id="group-name" value="{{ $item['id'] }}" name="id"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Заголовок</span>
-                            <input type="text" name="name" value="{{ $gel['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
+                            <input type="text" name="name" value="{{ $item['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Описание</span>
-                            <input type="text" name="description" value="{{ $gel['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
+                            <input type="text" name="description" value="{{ $item['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Состав</span>
-                            <input type="text" name="composition" value="{{ $gel['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
+                            <input type="text" name="composition" value="{{ $item['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
                         </div>
 
                         <div class="input-group mb-3">
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Цена</span>
-                                <input type="text" name="price" value="{{ $gel['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
+                                <input type="text" name="price" value="{{ $item['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Объем</span>
-                                <input type="text" name="volume" value="{{ $gel['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
+                                <input type="text" name="volume" value="{{ $item['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
                             </div>
                         </div>
 
@@ -219,59 +219,59 @@
     </div>
 
     <div id="list-scrabs" class="mt-4 d-none flex-column">
-        @foreach($scrabs as $scrab)
+        @foreach($scrabs as $item)
             <div class="mt-2 d-flex flex-wrap h-auto align-items-center justify-content-between border" style="height: 50px">
                 <div class="d-flex flex-row">
-                    <p class="m-2">id: {{ $scrab['id'] }}</p> <p class="m-2">{{ $scrab['name'] }}</p>
-                    <p class="m-2">({{ $scrab['volume'] }} мл)</p>
+                    <p class="m-2">id: {{ $item['id'] }}</p> <p class="m-2">{{ $item['name'] }}</p>
+                    <p class="m-2">({{ $item['volume'] }} мл)</p>
                 </div>
                 <div class="d-flex">
-                    <button id="btn-edit-scrab-{{ $scrab['id'] }}" class="btn btn-primary m-1">Редактировать</button>
+                    <button id="btn-edit-scrab-{{ $item['id'] }}" class="btn btn-primary m-1">Редактировать</button>
                     <form action="{{ url('admin/delete/') }}" method="POST" class="m-1">
                         @csrf
                         <input type="hidden" name="group-name" value="3">
-                        <input type="hidden" name="id" value="{{ $scrab['id'] }}">
+                        <input type="hidden" name="id" value="{{ $item['id'] }}">
                         <button class="btn btn-primary">Удалить</button>
                     </form>
                 </div>
             </div>
-            <div id="toggle-scrab-{{ $scrab['id'] }}" style="display: none">
+            <div id="toggle-scrab-{{ $item['id'] }}" style="display: none">
                 <div class="mt-2 d-flex align-items-center justify-content-between border-1">
                     <form action="{{ url('admin/edit/') }}" method="POST" class="row g-3 needs-validation">
                         @csrf
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Группа</span>
-                            <input class="form-control" type="text" id="group-name" value="{{ $scrab['category'] }}" name="group-name"><br><br>
+                            <input class="form-control" type="text" id="group-name" value="{{ $item['category'] }}" name="group-name"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">id</span>
-                            <input class="form-control" type="text" id="group-name" value="{{ $scrab['id'] }}" name="id"><br><br>
+                            <input class="form-control" type="text" id="group-name" value="{{ $item['id'] }}" name="id"><br><br>
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text" id="basic-addon1">Заголовок</span>
-                            <input type="text" name="name" value="{{ $scrab['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
+                            <input type="text" name="name" value="{{ $item['name'] }}" class="form-control" placeholder="Название товара" aria-label="Название товара" aria-describedby="basic-addon1">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Описание</span>
-                            <input type="text" name="description" value="{{ $scrab['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
+                            <input type="text" name="description" value="{{ $item['description'] }}" class="form-control" placeholder="Описание товара" aria-label="Описание товара">
                         </div>
 
                         <div class="input-group mb-3">
                             <span class="input-group-text">Состав</span>
-                            <input type="text" name="composition" value="{{ $scrab['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
+                            <input type="text" name="composition" value="{{ $item['composition'] }}" class="form-control" placeholder="Состав товара, условия хранения" aria-label="Состав товара, условия хранения">
                         </div>
 
                         <div class="input-group mb-3">
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Цена</span>
-                                <input type="text" name="price" value="{{ $scrab['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
+                                <input type="text" name="price" value="{{ $item['price'] }}" class="form-control" placeholder="Цена товара" aria-label="Цена товара" aria-describedby="basic-addon1">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text" id="basic-addon1">Объем</span>
-                                <input type="text" name="volume" value="{{ $scrab['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
+                                <input type="text" name="volume" value="{{ $item['volume'] }}" class="form-control" placeholder="Объем товара" aria-label="Объем товара" aria-describedby="basic-addon1">
                             </div>
                         </div>
 
