93232cf2b17a784eab297c669ade5874256b8dbe
Akllike <33116@list.ru>
2024-11-25 16:07:34 +0300

    25.11.2024-2

--- resources/views/Catalog/Faces/showFaces.blade.php
+++ resources/views/Catalog/Faces/showFaces.blade.php
@@ -53,7 +53,13 @@
                                 </svg>
                             </button>
                         </form>--}}
-                        <button type="submit" class="btn btn-outline-dark border-2 m-1 shadow">
+                        <div class="quantity-block" style="display: flex">
+                            <button class="btn quantity-minus" style="margin: 2px; width: 30px; height: 30px;">-</button>
+                            <p class="quantity" style="margin: 2px; font-size: 25px;">1</p>
+                            <button class="btn quantity-plus" style="margin: 2px; width: 30px; height: 30px;">+</button>
+                        </div>
+                        <!-- <input type="text" class="form-control quantity" placeholder="Количество" value="1"> -->
+                        <button type="submit" class="btn btn-outline-dark border-2 m-1 shadow add-cart">
                             В корзину
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16">
                                 <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
