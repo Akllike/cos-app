715a34d8a3299b0a0a74f05b016fee32eed63f55
Akllike <33116@list.ru>
2024-11-26 15:45:19 +0300

    26.11.2024.1

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <meta name="description" content="Косметика для твоей жизни ❤">
+    <meta property="og:image" content="{{ url('storage/img/navbar.jpg') }}" />
+    <meta property="og:url" content="{{ route('index') }}" />
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
