50efd764ef3949184225b72874fa798f7be8269a
Akllike <33116@list.ru>
2025-06-23 15:04:25 +0300

    23-06.2025-1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\CartService;
+use App\Services\TelegramService;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use App\Services\ProductsService;
@@ -15,6 +17,14 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->singleton(ProductsService::class, function ($app) {
             return new ProductsService();
+        });
+
+        $this->app->singleton(TelegramService::class, function ($app) {
+            return new TelegramService();
+        });
+
+        $this->app->singleton(CartService::class, function ($app) {
+            return new CartService();
         });
     }
 
